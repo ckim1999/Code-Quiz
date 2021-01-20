40934ef506a70d600d71fb3eba28f4b1559c943f
Christopher Kim <chrisnkim1999@gmail.com>
2021-01-19 21:57:05 -0500

    added a button action to start my countdown function

--- code.js
+++ code.js
@@ -3,12 +3,14 @@ var title = document.querySelector(".title");
 var intro = document.querySelector(".instructions");
 // select the time class and put it into a variable to use in the countdown function
 var time = document.querySelector(".time");
+// select the start id and convert it to a button
+var startBtn = document.getElementById("start");
 
 // create the starting time value to then decrement down
 seconds = 60;
 
 // countdown function
-function Countdown () {
+startBtn.addEventListener("click", function Countdown () {
     var timedown = setInterval(function() {
         // have the time decrease by 1
         seconds--;
@@ -21,8 +23,9 @@ function Countdown () {
             alert("Time is up")
         }
     }, 500);
-}
+})
 
+// Introduction function
 function Introduction () {
     title.textContent = "Welcome to My Code Quiz"
     intro.textContent = "This is my version of a coding quiz, this quiz will test your knowledge of basic HTML, CSS, and JavaScript. There are 7 questions and you will have 60 seconds to complete the quiz. When you are ready to my attempt of a coding quiz press the start button to begin."
